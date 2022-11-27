b20cc5246dab6244662606f8758665457de7b4d6
tohoho-2614 <komorebi2614@gmail.com>
2022-11-27 23:14:44 +0900

    ✨ Add windows setup file.

---
+++ .windows/install.ps1
@@ -0,0 +1,25 @@
+# scoopをインストール
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+iwr -useb get.scoop.sh | iex
+
+# git
+scoop install git
+
+#bucketを追加
+scoop bucket add versions
+scoop bucket add extras
+
+# vscode
+scoop install vscode
+
+# neovim
+scoop install neovim
+
+# zenhan
+scoop install zenhan
+
+# fzf
+scoop install fzf
+
+# python
+scoop install python
