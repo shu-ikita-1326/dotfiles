a5e0157ff371b9a286248ea000a2e5e7f83869fc
tohoho-2614 <komorebi2614@gmail.com>
2022-12-02 17:08:55 +0900

    ✨ Add plugin into nvim.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -94,6 +94,9 @@ return packer.startup(function(use)
   use({ "airblade/vim-gitgutter" })
   use({ "tpope/vim-fugitive" })
 
+  -- easymotion
+  use({ "easymotion/vim-easymotion" })
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
---
+++ .config/nvim/plugin/easymotion.lua
@@ -0,0 +1,1 @@
+vim.keymap.set("n", "s", "<Plug>(easymotion-overwin-f2)")
