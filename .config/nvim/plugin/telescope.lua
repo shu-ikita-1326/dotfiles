b5857e61f52a149b812581aacbac4ebfe1cf9ded
tohoho-2614 <komorebi2614@gmail.com>
2022-12-08 00:25:48 +0900

    Fix nvim plugin. telescope

--- .config/nvim/plugin/telescope.lua
+++ .config/nvim/plugin/telescope.lua
@@ -1,8 +1,21 @@
+local telescope = require('telescope')
+local actions = require('telescope.actions')
+
+require("telescope").load_extension "file_browser"
+
+telescope.setup {
+  defaults = {
+    mappings = {
+      n = {
+        ["<C-c>"] = actions.close
+      },
+    },
+  },
+}
+
 vim.keymap.set("n", "<Leader>ff", ":Telescope find_files<CR>")
 vim.keymap.set("n", "<Leader>gf", ":Telescope git_files<CR>")
 vim.keymap.set("n", "<Leader>gs", ":Telescope git_status<CR>")
 vim.keymap.set("n", "<Leader>gr", ":Telescope live_grep<CR>")
-vim.keymap.set("n", "<Leader>fe", ":Telescope file_browser<CR>")
+vim.keymap.set("n", "<Leader>fe", ":Telescope file_browser hidden=true initial_mode=normal<CR>")
 vim.keymap.set("n", "<Leader>h", ":Telescope oldfiles<CR>")
-
-require("telescope").load_extension "file_browser"
