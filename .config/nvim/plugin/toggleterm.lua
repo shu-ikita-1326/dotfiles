58046250a0baa4c34e58b6bcb2941a53d8e9273c
tohoho-2614 <komorebi2614@gmail.com>
2022-12-11 08:12:43 +0900

    Add nvim custom terminal lazydocker.

--- .config/nvim/plugin/toggleterm.lua
+++ .config/nvim/plugin/toggleterm.lua
@@ -13,5 +13,11 @@ function _lazygit_toggle()
   lazygit:toggle()
 end
 
+local lazydocker = Terminal:new({ cmd = "lazydocker", hidden = true, direction = "float"})
+
+function _lazydocker_toggle()
+  lazydocker:toggle()
+end
 vim.keymap.set("n", "<Leader>tig", ":lua _tig_toggle()<CR>", {noremap = true, silent = true})
 vim.keymap.set("n", "<Leader>lg", ":lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+vim.keymap.set("n", "<Leader>ld", ":lua _lazydocker_toggle()<CR>", {noremap = true, silent = true})
