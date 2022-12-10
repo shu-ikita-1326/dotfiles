b54370a155b70d34f398bc7b0ed3225c2e8092c8
tohoho-2614 <komorebi2614@gmail.com>
2022-12-10 23:46:28 +0900

    Add zsh alias and nvim custom terminal

--- .config/nvim/plugin/toggleterm.lua
+++ .config/nvim/plugin/toggleterm.lua
@@ -7,4 +7,11 @@ function _tig_toggle()
   tig:toggle()
 end
 
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float"})
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
 vim.keymap.set("n", "<Leader>tig", ":lua _tig_toggle()<CR>", {noremap = true, silent = true})
+vim.keymap.set("n", "<Leader>lg", ":lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
--- .config/zsh/rc/alias.zsh
+++ .config/zsh/rc/alias.zsh
@@ -7,6 +7,7 @@ alias t='tmux'
 if [ -n "$TMUX" ]; then
 	alias ide_v='tmux split-window -h -p 15; tmux split-window -v -p 66; tmux split-window -v -p 50; tmux select-pane -L; nvim;'
 	alias ide_s='tmux split-window -v -p 15; tmux split-window -h -p 66; tmux split-window -h -p 50; tmux select-pane -U; nvim;'
+	alias fzf='fzf-tmux -p80%'
 fi
 
 # git
@@ -15,6 +16,8 @@ alias ga='git add'
 alias gst='git status'
 alias gsf='git-switch-fzf'
 
+alias lg='lazygit'
+
 # ls
 alias ls='ls --color=auto'
 alias la='ls -a'
@@ -22,6 +25,8 @@ alias ll='ls -la'
 
 # docker
 alias dowatch='watch -t \''docker ps --all --format \"{{.Names}}: {{.Status}}\"\'''
+
+alias ld='lazydocker'
 
 # docker-compose
 alias dc='docker-compose'
