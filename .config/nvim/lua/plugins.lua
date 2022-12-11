9e6d2e0b99811e8ce637a74515b22eb9672a6f20
tohoho-2614 <komorebi2614@gmail.com>
2022-12-11 23:45:47 +0900

    Add nvim plugin. translate.vim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -101,6 +101,7 @@ return packer.startup(function(use)
   -- utility
   use({ "machakann/vim-sandwich" })
   use({ "akinsho/toggleterm.nvim" })
+  use({ "skanehira/translate.vim" })
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
---
+++ .config/nvim/plugin/translate.lua
@@ -0,0 +1,2 @@
+local opt = { silent=true, noremap=true}
+vim.keymap.set("v", "<Leader>tr", ":Translate<CR>", opt)
