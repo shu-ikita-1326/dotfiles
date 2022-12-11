9724e7b3a093a20e07276b14609380197a80cc57
tohoho-2614 <komorebi2614@gmail.com>
2022-12-11 23:38:07 +0900

    Fix telescope keymaps.

--- .config/nvim/plugin/telescope.lua
+++ .config/nvim/plugin/telescope.lua
@@ -1,5 +1,6 @@
 local telescope = require('telescope')
 local actions = require('telescope.actions')
+local opt = { silent=true, noremap=true}
 
 require("telescope").load_extension "file_browser"
 
@@ -13,9 +14,9 @@ telescope.setup {
   },
 }
 
-vim.keymap.set("n", "<Leader>ff", ":Telescope find_files<CR>")
-vim.keymap.set("n", "<Leader>gf", ":Telescope git_files<CR>")
-vim.keymap.set("n", "<Leader>gs", ":Telescope git_status<CR>")
-vim.keymap.set("n", "<Leader>gr", ":Telescope live_grep<CR>")
-vim.keymap.set("n", "<Leader>fe", ":Telescope file_browser hidden=true initial_mode=normal<CR>")
-vim.keymap.set("n", "<Leader>h", ":Telescope oldfiles<CR>")
+vim.keymap.set("n", "<Leader>ff", ":Telescope find_files<CR>", opt)
+vim.keymap.set("n", "<Leader>gf", ":Telescope git_files<CR>", opt)
+vim.keymap.set("n", "<Leader>gs", ":Telescope git_status<CR>", opt)
+vim.keymap.set("n", "<Leader>gr", ":Telescope live_grep<CR>", opt)
+vim.keymap.set("n", "<Leader>fe", ":Telescope file_browser hidden=true initial_mode=normal<CR>", opt)
+vim.keymap.set("n", "<Leader>h", ":Telescope oldfiles<CR>", opt)
