47b215b1c9a5372914bf8958781d9c22b2e06ba8
tohoho-2614 <komorebi2614@gmail.com>
2022-12-30 00:03:56 +0900

    Add snippets.

---
+++ .config/nvim/snippets/javascriptreact.lua
@@ -0,0 +1,16 @@
+return {
+  s("cp", {
+    t("export const "),
+    i(1, "myComponent"),
+    t({" = () => {", "\treturn (", "\t\t"}),
+    i(2),
+    i(0),
+    t({"","\t)", "}"})
+  }),
+  s("class", {
+    t('className="'),
+    i(1),
+    t('"'),
+    i(0)
+  })
+}
