488c5d07105afe87506583b71c9e65ffdc7d1b8e
tohoho-2614 <komorebi2614@gmail.com>
2022-12-30 00:06:40 +0900

    Fix nvim plugin config. treesitter

---
+++ .config/nvim/plugin/treesitter.lua
@@ -0,0 +1,18 @@
+local ts = require('nvim-treesitter.configs')
+
+ts.setup {
+  highlight = {
+    enable = false,
+    disable = {},
+  },
+  indent = {
+    enable = true,
+    disable = {},
+  },
+  autotag = {
+    enable = true,
+  }
+}
+
+local paser_config = require 'nvim-treesitter.parsers'.get_parser_configs()
+paser_config.tsx.filetype_to_parsername = { 'javascript', 'typescript.tsx'}
