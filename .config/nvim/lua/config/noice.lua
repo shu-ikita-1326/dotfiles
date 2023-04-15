90f47403ff14fd6cc4193779220d57ba86146d61
shutils <komorebi2614@gmail.com>
2023-04-16 00:39:37 +0900

    Add noice disable autocmd

--- .config/nvim/lua/config/noice.lua
+++ .config/nvim/lua/config/noice.lua
@@ -1,3 +1,6 @@
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
 require("noice").setup({
   -- add any options here
   lsp = {
@@ -13,10 +16,33 @@ require("noice").setup({
   },
   -- you can enable a preset for easier configuration
   presets = {
-    bottom_search = true, -- use a classic bottom cmdline for search
-    command_palette = false, -- position the cmdline and popupmenu together
+    bottom_search = true,         -- use a classic bottom cmdline for search
+    command_palette = false,      -- position the cmdline and popupmenu together
     long_message_to_split = true, -- long messages will be sent to a split
-    inc_rename = false, -- enables an input dialog for inc-rename.nvim
-    lsp_doc_border = false, -- add a border to hover docs and signature help
+    inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false,       -- add a border to hover docs and signature help
   },
+})
+
+-- Temporarily turn off noise when using mason's search function
+augroup("noiceDisable", { clear = true })
+autocmd("FileType", {
+  group = "noiceDisable",
+  pattern = "mason",
+  command = ":Noice disable",
+})
+
+augroup("noiceEnable", { clear = true })
+autocmd("FileType", {
+  group = "noiceEnable",
+  pattern = "mason",
+  callback = function()
+    autocmd("BufLeave", {
+      group = "noiceEnable",
+      pattern = "*",
+      command = ":Noice enable",
+      once = true,
+    })
+  end,
+  nested = true,
 })
