d77d296ecc6600988467ed1f5d9750c41fce07d6
shutils <komorebi2614@gmail.com>
2023-05-15 00:19:02 +0900

    update other
    - add yank method win32yank.exe
    - change zsh keybind

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,4 +1,5 @@
 local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
 if vim.env.zenhan then
   autocmd("InsertLeave", {
     pattern = "*",
@@ -9,12 +10,6 @@ end
 autocmd("TextYankPost", {
   pattern = "*",
   command = ":lua vim.highlight.on_yank {higroup='IncSearch', timeout=300, on_visual=true}",
-})
-
--- Todo: osによって動作を切り替える
-autocmd("TextYankPost", {
- pattern = "*",
-  command = ":call system('iconv -f UTF-8 -t UTF-16 | clip.exe', @0)",
 })
 
 autocmd("QuickfixCmdPost", {
@@ -41,3 +36,32 @@ autocmd("WinEnter", {
   pattern = "term://*",
   command = "startinsert",
 })
+
+augroup("YankSetup", { clear = true })
+autocmd("VimEnter", {
+  group = "YankSetup",
+  pattern = "*",
+  callback = function()
+    -- Use win32yank.exe for yank if the execution environment is wsl
+    if vim.fn.executable('wslpath') == 1 then
+      if vim.fn.executable('win32yank.exe') == 1 then
+        vim.api.nvim_command('set clipboard+=unnamedplus')
+        vim.g.clipboard = {
+          name = 'myClipboard',
+          copy = {
+            ['+'] = 'win32yank.exe -i',
+            ['*'] = 'win32yank.exe -i',
+          },
+          paste = {
+            ['+'] = 'win32yank.exe -o',
+            ['*'] = 'win32yank.exe -o',
+          },
+        }
+      else
+        vim.notify("Install win32yank.exe if you want to share yanked code to the Windows clipboard. win32yank HP:(https://github.com/equalsraf/win32yank)",
+        vim.log.levels.INFO)
+      end
+    end
+  end,
+})
+
