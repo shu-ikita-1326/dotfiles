f7090562dbc1bc58d1c83e0a8ea36f9d795e642e
shutils <komorebi2614@gmail.com>
2023-11-25 10:53:41 +0900

    fix install script

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ IGNORE_PATTERN="^\.(git|windows|stylua.toml|luarc.json)"
 echo "Create dotfile links."
 for dotfile in .??*; do
     [[ $dotfile =~ $IGNORE_PATTERN ]] && continue
-    if [ -d "$dotfile" ] || [ -f "$dotfile" ]; then
+    if [ -d "$HOME/$dotfile" ] || [ -f "$HOME/$dotfile" ]; then
         mv -v "$HOME/$dotfile" "$HOME/${dotfile}.bak"
         ln -snfv "$(pwd)/$dotfile" "$HOME/$dotfile"
     else
