e0d62959dc0c1b034977f9f948ac41e7a81d715b
shutils <komorebi2614@gmail.com>
2024-01-29 16:47:46 +0900

    update: nvim plugin setting

--- .config/nvim/lua/config/gitlaber.lua
+++ .config/nvim/lua/config/gitlaber.lua
@@ -4,7 +4,7 @@ local opt = { silent = true, noremap = true, buffer = true }
 vim.g.gitlaber_config = {
   default_keymap_disable = true,
 }
-vim.keymap.set("n", "<Leader>gl", "<Cmd>Gitlaber<CR>", opt)
+vim.keymap.set("n", "<Leader>gl", "<Cmd>Gitlaber<CR>", { silent = true, noremap = true })
 
 function _G.gitlaber_project_status_keymap()
   vim.keymap.set("n", "i", "<Plug>(gitlaber-action-issue-list)", opt)
@@ -48,6 +48,20 @@ function _G.gitlaber_mr_list_keymap()
   vim.keymap.set("n", "P", "<Plug>(gitlaber-action-mr-preview)", opt)
   vim.keymap.set("n", "n", "<Plug>(gitlaber-action-mr-list-next)", opt)
   vim.keymap.set("n", "p", "<Plug>(gitlaber-action-mr-list-prev)", opt)
+  vim.keymap.set("n", "d", "<Plug>(gitlaber-action-mr-change-list)", opt)
+end
+
+function _G.gitlaber_mr_change_list_keymap()
+  vim.keymap.set("n", "q", "<Plug>(gitlaber-action-buffer-close)", opt)
+  vim.keymap.set("n", "<CR>", "<Plug>(gitlaber-action-mr-change-diff)", opt)
+end
+
+function _G.gitlaber_diff_new_keymap()
+  vim.keymap.set("n", "q", "<Plug>(gitlaber-action-buffer-close)", opt)
+end
+
+function _G.gitlaber_diff_old_keymap()
+  vim.keymap.set("n", "q", "<Plug>(gitlaber-action-buffer-close)", opt)
 end
 
 function _G.gitlaber_branch_list_keymap()
@@ -66,9 +80,40 @@ function _G.gitlaber_wiki_list_keymap()
   vim.keymap.set("n", "p", "<Plug>(gitlaber-action-wiki-list-prev)", opt)
 end
 
+function _G.gitlaber_issue_preview_keymap()
+  vim.keymap.set("n", "q", "<Plug>(gitlaber-action-buffer-close)", opt)
+end
+
+function _G.gitlaber_issue_edit_keymap()
+  vim.keymap.set("n", "q", "<Plug>(gitlaber-action-buffer-close)", opt)
+  vim.keymap.set("n", "<C-s>", "<Plug>(gitlaber-action-issue-edit-submit)", opt)
+end
+
+function _G.gitlaber_mr_preview_keymap()
+  vim.keymap.set("n", "q", "<Plug>(gitlaber-action-buffer-close)", opt)
+end
+
+function _G.gitlaber_mr_edit_keymap()
+  vim.keymap.set("n", "q", "<Plug>(gitlaber-action-buffer-close)", opt)
+  vim.keymap.set("n", "<C-s>", "<Plug>(gitlaber-action-mr-edit-submit)", opt)
+end
+
+function _G.gitlaber_wiki_preview_keymap()
+  vim.keymap.set("n", "q", "<Plug>(gitlaber-action-buffer-close)", opt)
+end
+
+function _G.gitlaber_wiki_edit_keymap()
+  vim.keymap.set("n", "q", "<Plug>(gitlaber-action-buffer-close)", opt)
+  vim.keymap.set("n", "<C-s>", "<Plug>(gitlaber-action-wiki-edit-submit)", opt)
+end
+
 function _G.gitlaber_ui_select_keymap()
   vim.keymap.set("n", "q", "<Plug>(gitlaber-action-buffer-close)", opt)
   vim.keymap.set("n", "<CR>", "<Plug>(gitlaber-action-ui-select)", opt)
+end
+
+function _G.gitlaber_mr_discussion_keymap()
+  vim.keymap.set("n", "q", "<Plug>(gitlaber-action-buffer-close)", opt)
 end
 
 autocmd("FileType", {
@@ -87,6 +132,21 @@ autocmd("FileType", {
 })
 
 autocmd("FileType", {
+  pattern = "*gitlaber-mr-change-list*",
+  command = "lua gitlaber_mr_change_list_keymap()",
+})
+
+autocmd("FileType", {
+  pattern = "*gitlaber-diff-new*",
+  command = "lua gitlaber_diff_new_keymap()",
+})
+
+autocmd("FileType", {
+  pattern = "*gitlaber-diff-old*",
+  command = "lua gitlaber_diff_old_keymap()",
+})
+
+autocmd("FileType", {
   pattern = "*gitlaber-branch-list*",
   command = "lua gitlaber_branch_list_keymap()",
 })
@@ -97,6 +157,41 @@ autocmd("FileType", {
 })
 
 autocmd("FileType", {
+  pattern = "*gitlaber-issue-preview*",
+  command = "lua gitlaber_issue_preview_keymap()",
+})
+
+autocmd("FileType", {
+  pattern = "*gitlaber-issue-edit*",
+  command = "lua gitlaber_issue_edit_keymap()",
+})
+
+autocmd("FileType", {
+  pattern = "*gitlaber-mr-preview*",
+  command = "lua gitlaber_mr_preview_keymap()",
+})
+
+autocmd("FileType", {
+  pattern = "*gitlaber-mr-edit*",
+  command = "lua gitlaber_mr_edit_keymap()",
+})
+
+autocmd("FileType", {
+  pattern = "*gitlaber-wiki-preview*",
+  command = "lua gitlaber_wiki_preview_keymap()",
+})
+
+autocmd("FileType", {
+  pattern = "*gitlaber-wiki-edit*",
+  command = "lua gitlaber_wiki_edit_keymap()",
+})
+
+autocmd("FileType", {
   pattern = "*gitlaber-ui-select*",
   command = "lua gitlaber_ui_select_keymap()",
+})
+
+autocmd("FileType", {
+  pattern = "*gitlaber-mr-discussion*",
+  command = "lua gitlaber_mr_discussion_keymap()",
 })
