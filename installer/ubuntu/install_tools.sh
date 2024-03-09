1c372230d11e6d8e6a1dd84bbd7cd586effbe8eb
shutils <komorebi2614@gmail.com>
2024-03-09 09:59:05 +0900

    update: add xclip

--- installer/ubuntu/install_tools.sh
+++ installer/ubuntu/install_tools.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# パッケージマネージャを最新に更新
 echo 'apt updating...'
 sudo -E apt update > /dev/null
 
-# for Telescope in nvim
+# For Telescope in nvim
 if ! (type "sqlite" > /dev/null 2>&1); then
   echo 'Installing sqlite...'
   sudo -E apt install sqlite3 libsqlite3-dev > /dev/null
@@ -13,3 +12,6 @@ fi
 brew bundle --file ~/dotfiles/installer/ubuntu/Brewfile
 
 mise install
+
+# For share clicpboard
+sudo apt -y install xclip
